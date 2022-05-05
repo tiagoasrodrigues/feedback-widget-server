aceb680e20cf29a43e49991e81bcfb36caa02b34
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-05-05 10:43:58 -0300

    :wrench: Setup Prisma

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.listen(3334, () => {
+app.get("/users", (req, res) => {
+  return res.send("Hello World!")
+})
+
+app.listen(3333, () => {
   console.log("HTTP server running")
 })
